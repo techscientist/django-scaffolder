902481bc89e5f5091cc497e4316ad3867b28af33
Uri Zolotko <yurii.zolotko@gmail.com>
2016-11-07 20:36:06 +0700

    error: unrecognized arguments when running the script for creating a new model #1

--- generate_scaffold/utils/cacheclear.py
+++ generate_scaffold/utils/cacheclear.py
@@ -13,13 +13,12 @@ def reload_django_appcache():
     cache.handled = {}
     cache.loaded = False
 
-    for app in cache.get_apps():
-        __import__(app.__name__)
-        reload(app)
+    for app in cache.get_app_configs():
+        __import__(app.name)
+        reload(app.module)
 
 
 def clean_pyc_in_dir(dirpath):
     for root, _, files in os.walk(dirpath):
         for f in [f for f in files if os.path.splitext(f)[-1] == '.pyc']:
             os.remove(os.path.join(root, f))
-
